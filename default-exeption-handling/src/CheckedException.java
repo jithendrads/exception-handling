4a139e627f1ddfe6041d2cd255ce9e7e72418dc5
Jithendra <jithendrads@gmail.com>
2021-01-06 14:27:04 +0530

    Checked Exception example

---
+++ default-exeption-handling/src/CheckedException.java
@@ -0,0 +1,13 @@
+import java.io.PrintWriter;
+
+public class CheckedException {
+
+	public static void main(String[] args) {
+		//PrintWriter pw=new PrintWriter("abc.txt");
+		//pw.println("Hello");
+		System.out.println("I want to sleep peacefully");
+		Thread.sleep(1000);
+
+	}
+
+}
